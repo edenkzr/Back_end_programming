1b2a83d6671a9a9d5825a234d4ef2dbe824e4bd7
dcuadro <dcuadro@wgu.edu>
2024-05-01 16:10:34 -0700

    created dto purchase and purchaseResponse for Task F

---
+++ 02-BackEnd/spring-boot-ecommerce/src/main/java/com/dto/Purchase.java
@@ -0,0 +1,17 @@
+package com.dto;
+
+import com.entities.Cart;
+import com.entities.CartItem;
+import com.entities.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class Purchase {
+    private Customer customer;
+    private Cart cart;
+    private Set<CartItem> cartItems;
+}
